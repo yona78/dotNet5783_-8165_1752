59d3017f57222d6310ffbd6f7d17a7a467b394f8
yona78 <yonaorunov1@gmail.com>
2022-10-26 18:03:25 +0300

    welcome functions

---
+++ dotNet5783_-8165_1752/Stage0/Progrem8165.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class FirstProgram
+    {
+        static void Main(string[] args)
+        {
+            Welcome8165();
+            Welcome1752();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1752();
+        private static void Welcome8165()
+        {
+            Console.Write("what is your name: ");
+            String x = Console.ReadLine();
+            Console.WriteLine("{0} is a good name", x);
+        }
+
+    }
+}
