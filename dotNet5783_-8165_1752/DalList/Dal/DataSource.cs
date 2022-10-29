0db3c3a6335cc2c5ac60a9292158c2725682a620
avishaigonen123 <avishaigonen@gmail.com>
2022-10-30 00:50:40 +0300

    I've finished the DataSource

--- dotNet5783_-8165_1752/DalList/Dal/DataSource.cs
+++ dotNet5783_-8165_1752/DalList/Dal/DataSource.cs
@@ -1,17 +1,15 @@
 ﻿using DO;
 using System.ComponentModel;
-
 namespace Dal;
 internal static class DataSource
 {
 
-
     static readonly Random rnd = new Random();
-    static internal DalOrder[] orders = new DalOrder[100];
+    static internal Order[] orders = new Order[100];
     static int maxOrder = 100;
-    static internal DalProduct[] products = new DalProduct[50];
+    static internal Product[] products = new Product[50];
     static int maxProducts = 50;
-    static internal DalOrderItem[] OrderItems = new DalOrderItem[200];
+    static internal OrderItem[] orderItems = new OrderItem[200];
     static int maxOrderItems = 200;
 
 
@@ -21,20 +19,20 @@ internal static class DataSource
         s_Initialize();
     }
 
-    private static void addOrder(DalOrder newOrder)
+    private static void addOrder(Order newOrder)
     {
 
         orders[Config.AmountOrders] = newOrder;
         Config.AmountOrders++;
     }
 
-    private static void addProduct(DalProduct newProduct)
+    private static void addProduct(Product newProduct)
     {
         products[Config.AmountProducts] = newProduct;
         Config.AmountProducts++;
     }
 
-    private static void addOrdersItem(DalOrderItem newOrderItem)
+    private static void addOrdersItem(OrderItem newOrderItem)
     {
         OrderItems[Config.AmountOrderItems] = newOrderItem;
         Config.AmountOrderItems++;
@@ -46,11 +44,107 @@ internal static class DataSource
         const int ProductInit = 10;
         const int OrderInit = 20;
         const int OrderItemslInit = 40;
-        for (int i = 0; i < ProductInit; i++)
+
+        /// Products initializetion
+        int[] arrayOfRandomNumbersForProducts = new int[ProductInit];
+        /// the next code will check whether there are duplicate Id's
+        bool checkForDuplicateId = false;
+        do
         {
-            DalProduct X = new DalProduct();
-            Id = RandomGen.Next(100000, 999999),
+            checkForDuplicateId = false;
+            for (int i = 0; i < ProductInit; i++)
+            {
+                arrayOfRandomNumbersForProducts[i] = rnd.Next(100000, 999999);
             }
+            for (int i = 0; i < ProductInit; i++)
+            {
+                int currentId = arrayOfRandomNumbersForProducts[i];
+                for (int j = i + 1; j < ProductInit; j++)
+                {
+                    checkForDuplicateId = checkForDuplicateId || (arrayOfRandomNumbersForProducts[j] == currentId);
+                }
+            }
+        } while (!checkForDuplicateId);
+        /// there must be two products with empty amount (InStock=0)
+        Product product;
+        product = new Product { ID = arrayOfRandomNumbersForProducts[0], Name = "Black coat", Category = Enums.Category.Coats, InStock = 50, Price = 399.99 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[1], Name = "Winter dress", Category = Enums.Category.Dresses, InStock = 30, Price = 150 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[2], Name = "Cowboy hat", Category = Enums.Category.Hats, InStock = 20, Price = 30.99 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[3], Name = "Sport shoes", Category = Enums.Category.Shoes, InStock = 39, Price = 299.99 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[4], Name = "White socks", Category = Enums.Category.Socks, InStock = 24, Price = 15 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[5], Name = "Striped skirt", Category = Enums.Category.Skirts, InStock = 0, Price = 70.99 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[6], Name = "Blue shirt", Category = Enums.Category.Shirts, InStock = 12, Price = 35 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[7], Name = "Red shirt", Category = Enums.Category.Shirts, InStock = 61, Price = 35};
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[8], Name = "Sha'abeth shoes", Category = Enums.Category.Shoes, InStock = 17, Price = 200 };
+        addProduct(product);
+        product = new Product { ID = arrayOfRandomNumbersForProducts[9], Name = "Goofy hat", Category = Enums.Category.Hats, InStock = 28, Price = 25.99};
+        addProduct(product);
+
+
+        /// Orders initializetion
+        string[] customerNames = new string[10] { "Yossi", "Chaim", "David", "Ariel", "Yona", "Avishai", "Binyamin", "Noam", "Ori", "Moshe" };
+        string[] cities = new string[5] { "Jerusalem", "Tel Aviv", "Haifa", "Beer Sheva", "Petah Tiqwa" };
+        string[] streets = new string[10] { "Arlozoroff", "Balfour", "Begin", "Ben Gurion", "Ben Yehuda", "Bialik", "Herzl", "Ibn Gabirol", "Jabotinsky", "Herzl\r\n" };
+
+        DateTime[] orderDates = new DateTime[OrderInit];
+        DateTime[] shipDataDates = new DateTime[OrderInit];
+        DateTime[] deliveryDataDates = new DateTime[OrderInit];
+        for (int i = 0; i < OrderInit; i++)
+        {
+            TimeSpan duration = new TimeSpan(-rnd.Next(50, 200), rnd.Next(24), rnd.Next(60), rnd.Next(60));
+            orderDates[i] = DateTime.Now.Add(duration);
+            duration = new TimeSpan(rnd.Next(20, 45), rnd.Next(24), rnd.Next(60), rnd.Next(60));
+            shipDataDates[i] = orderDates[i].Add(duration);
+            if (i % 5 == 0 || i % 4 == 0 || i % 3 == 0) // thats equal to 60% from all of the orders
+            {
+                duration = new TimeSpan(rnd.Next(2, 18), rnd.Next(24), rnd.Next(60), rnd.Next(60));
+                shipDataDates[i] = orderDates[i].Add(duration);
+            }
+        }
+        for (int i = 0; i < OrderInit; i++)
+        {
+            Order order = new Order { ID = Config.AmountOrders, CustomerName = customerNames[rnd.Next(10)], CustomerEmail = customerNames[rnd.Next(10)] + rnd.Next(1000) + "@gmail.com", CustomerAdress = rnd.Next(100) + " " + streets[rnd.Next(10)] + " Street '\t' " + rnd.Next(1000000, 9999999) + " " + cities[rnd.Next(5)] + " '\t' Israel", OrderDate = orderDates[i], ShipData = shipDataDates[i], DeliveryrData = deliveryDataDates[i] };
+            addOrder(order);
+        }
+
+        /// OrderItems initializetion
+        for (int i = 0; i < OrderInit; i++)
+        {
+            /// checking whether there are enught items from the spiecific product
+            int productInOrder = rnd.Next(ProductInit);
+            int amountOfItems = rnd.Next(1, 4);
+            if (i % 4 == 0) /// by this way, i insure that there would be at least 40 items. because 5*4 = 20 + 15*1 = 35. however, in the 15 other loops it's between 1 to 4, so it very likely it would be bigger than 20. 
+            {
+                amountOfItems = 4;
+                do
+                {
+                    if (products[productInOrder].InStock == 0)
+                        productInOrder = rnd.Next(ProductInit);
+                } while (products[productInOrder].InStock <= amountOfItems || products[productInOrder].InStock == 0);
+
+            }
+            do
+            {
+                while (products[productInOrder].InStock <= amountOfItems && products[productInOrder].InStock != 0)
+                    amountOfItems = rnd.Next(1, 4);
+                if (products[productInOrder].InStock == 0)
+                    productInOrder = rnd.Next(ProductInit);
+            } while (products[productInOrder].InStock <= amountOfItems || products[productInOrder].InStock == 0);
+
+            OrderItem orderItem = new OrderItem { OrderItemID = Config.AmountOrderItems, ProductID = products[productInOrder].ID, OrderID = orders[i].ID, Price = products[productInOrder].Price, Amount = amountOfItems };
+            products[productInOrder].InStock -= amountOfItems;
+            addOrdersItem(orderItem);
+        }
+
+
     }
 
     internal class Config
@@ -61,8 +155,8 @@ internal static class DataSource
         static internal int AmountProducts = 0;
         static internal int IdCreations = 0;
 
-        private static int LastIndexOrder = 0;
-        private static int LastIndexOrderItems= 0;
+        private static int LastIndexOrder = 20;
+        private static int LastIndexOrderItems = 40;
 
         public int getLastIndexOrder { get => LastIndexOrder++; }
         public int getLastIndexOrderItems { get => LastIndexOrderItems++; }
